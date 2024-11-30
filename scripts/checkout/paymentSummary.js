bc71a44dde709646dfd4b454c3e19dcc1a13e74e
adis14 <adityasutar014@gmail.com>
2024-11-30 15:15:52 +0530

    first commit

---
+++ scripts/checkout/paymentSummary.js
@@ -0,0 +1,51 @@
+// import {cart} from '../../data/cart.js';
+import {cart, resetCart} from '../../data/cart.js';
+import {getProduct} from '../../data/products.js';
+import {getDeliveryOption} from '../../data/deliveryOptions.js';
+import {formatCurrency} from '../utils/money.js';
+import {addOrder} from '../../data/orders.js';
+export function renderPaymentSummary() {
+  let productPriceCents = 0;
+  let shippingPriceCents = 0;
+  cart.forEach((cartItem) => {
+    const product = getProduct(cartItem.productId);
+    productPriceCents += product.priceCents * cartItem.quantity;
+    const deliveryOption = getDeliveryOption(cartItem.deliveryOptionId);
+    shippingPriceCents += deliveryOption.priceCents;
+  });
+  const totalBeforeTaxCents = productPriceCents + shippingPriceCents;
+  const taxCents = totalBeforeTaxCents * 0.1;
+  const totalCents = totalBeforeTaxCents + taxCents;
+  const paymentSummaryHTML = `
+    <div class="payment-summary">
+
+@@ -69,26 +69,28 @@
+
+  document.querySelector('.js-payment-summary')
+    .innerHTML = paymentSummaryHTML;
+
+  document.querySelector('.js-place-order')
+    .addEventListener('click', async () => {
+      try {
+        const response = await fetch('https://supersimplebackend.dev/orders', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify({
+            cart: cart
+          })
+        });
+
+        const order = await response.json();
+        addOrder(order);
+
+      } catch (error) {
+        console.log('Unexpected error. Try again later.');
+      }
+
+      // Extra feature: make the cart empty after creating an order.
+      resetCart();
+      window.location.href = 'orders.html';
+    });`
+}
