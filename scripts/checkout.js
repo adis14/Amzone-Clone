ce31a58963ea4d740a56f063c2a08dfd47a7b362
adis14 <adityasutar014@gmail.com>
2024-11-30 15:27:20 +0530

    checkout.js

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -2,8 +2,7 @@ import {renderOrderSummary} from './checkout/orderSummary.js';
 import {renderPaymentSummary} from './checkout/paymentSummary.js';
 import {loadProducts, loadProductsFetch} from '../data/products.js';
 import {loadCart, loadCartFetch} from '../data/cart.js';
-// import '../data/cart-class.js';
-// import '../data/backend-practice.js';
+
 async function loadPage() {
   try {
     // throw 'error1';
